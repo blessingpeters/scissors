1dbccf9701725ec7d892624269c95818cfcfa668
Blessing Peter <cherubpeters@gmail.com>
2023-10-13 00:59:40 +0100

    fix build error

--- components/common/AuthButton.tsx
+++ components/common/AuthButton.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
 import { signIn } from 'next-auth/react';
@@ -14,7 +15,7 @@ export const GoogleBtn = () => {
     return (
         <button 
             className='flex items-center text-white bg-blue-600 px-6 py-2 rounded-md cursor-pointer' 
-            type='button' 
+            type='button'
             onClick={() => handleSignIn('google')}
         >
             <Image className='mr-4' src="/images/google.svg" alt="google logo" width={20} height={19} />
@@ -27,7 +28,7 @@ export const AppleBtn = () => {
     return (
         <button 
             className='flex items-center text-white bg-black px-6 py-2 rounded-md cursor-pointer' 
-            type='button' 
+            type='button'
             onClick={() => handleSignIn('apple')} // Make sure to call the correct provider
         >
             <Image className='mr-4' src="/images/apple.svg" alt="apple logo" width={20} height={19} /> {/* Update the source here */}
